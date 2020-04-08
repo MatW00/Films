87b23bf72cfd6e9a6747bfd5ac0b64c03cfea18a
unknown <sophiearnou@yahoo.fr>
2020-04-08 09:28:21 +0200

    bb
    nn

--- template.php
+++ template.php
@@ -26,7 +26,7 @@
 
                 <ul class="navbar-nav mr-auto mt-2 mt-lg-0">
                     <li class="nav-item">
-                        <a class="nav-link" href="#">Accueil___</a>
+                        <a class="nav-link" href="#">Accueil</a>
                     </li>
                     <li class="nav-item">
                         <a class="nav-link" href="#">Catégories</a>
